7961d91ac48afa59aa72826886e6b4de3ba3257e
walkermannum1 <2469377884@qq.com>
2016-09-21 17:51:10 +0800

    Use google play service.

--- app/src/main/java/com/example/user/locatr/LocatrFragment.java
+++ app/src/main/java/com/example/user/locatr/LocatrFragment.java
@@ -1,13 +1,18 @@
 package com.example.user.locatr;
 
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationServices;
 
 /**
  * Created by user on 2016/9/21.
@@ -15,6 +20,7 @@ import android.widget.ImageView;
 
 public class LocatrFragment extends Fragment {
     private ImageView mImageView;
+    private GoogleApiClient mClient;
 
     public static LocatrFragment newInstance() {
         return new LocatrFragment();
@@ -24,6 +30,19 @@ public class LocatrFragment extends Fragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setHasOptionsMenu(true);
+
+        mClient = new GoogleApiClient.Builder(getActivity()).addApi(LocationServices.API)
+                .addConnectionCallbacks(new GoogleApiClient.ConnectionCallbacks() {
+                    @Override
+                    public void onConnected(Bundle bundle) {
+                        getActivity().invalidateOptionsMenu();
+                    }
+
+                    @Override
+                    public void onConnectionSuspended(int i) {
+
+                    }
+                }).build();
     }
 
     @Override
@@ -34,8 +53,26 @@ public class LocatrFragment extends Fragment {
     }
 
     @Override
+    public void onStart() {
+        super.onStart();
+
+        getActivity().invalidateOptionsMenu();
+        mClient.connect();
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+
+        mClient.disconnect();
+    }
+
+    @Override
     public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
         super.onCreateOptionsMenu(menu, inflater);
         inflater.inflate(R.menu.fragment_locatr, menu);
+
+        MenuItem searchItem = menu.findItem(R.id.action_locate);
+        searchItem.setEnabled(mClient.isConnected());
     }
 }
