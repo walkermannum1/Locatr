b06e2cc5a5a80ad424d936b17bf9e20997774244
walkermannum1 <2469377884@qq.com>
2016-09-21 11:24:43 +0800

    Initial commit

---
+++ app/src/main/java/com/example/user/locatr/LocatrActivity.java
@@ -0,0 +1,13 @@
+package com.example.user.locatr;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class LocatrActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.fragment_locatr);
+    }
+}
